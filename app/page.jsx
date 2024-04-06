372d1c67c22100a768657cac8b9ac5ac9e974f28
Mukul saini <133125833+sainimukul1911@users.noreply.github.com>
2024-04-06 15:13:01 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -30,6 +30,18 @@ function HomePage() {
     generationConfig: {
       maxOutputTokens: 1000,
     },
+    safetySetting: [
+      { category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE' },
+      {
+        category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
+        threshold: 'BLOCK_NONE'
+      },
+      { category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE' },
+      {
+        category: 'HARM_CATEGORY_DANGEROUS_CONTENT',
+        threshold: 'BLOCK_NONE'
+      }
+      ],
   });
 
 
